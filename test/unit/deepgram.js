ac03064a78684076b67aaa790de52a23ae36ce07
Matias Insaurralde <matias@insaurral.de>
2016-03-16 05:48:32 -0300

    Updating tests

--- test/unit/deepgram.js
+++ test/unit/deepgram.js
@@ -9,12 +9,49 @@ describe( 'Deepgram API', function() {
   const Deepgram = require( '../../lib/deepgram' ).default,
         deepgram = new Deepgram({ userId: process.env.DEEPGRAM_USER_ID })
 
+  var testContentId = '1458116570-d27af336-b738-4757-88ac-607e5eae970a-3584494445',
+      testContentUrl = 'https://github.com/pixelate/soundcontrol/raw/master/assets/mp3/HelloWorld.mp3',
+      testContentQuery = 'hello'
+
   it( 'should check the balance', function( done ) {
-    deepgram.getBalance().then( data => {
-      var body = data.body
-      body.should.be.an( 'object' )
-      body.should.have.any.key( 'balance' )
+    deepgram.getBalance().then( balance => {
+      balance.should.be.a( 'number' )
       done()
     })
   })
+
+  it( 'should index content', (done) => {
+    deepgram.indexContent( testContentUrl ).then( contentID => {
+      contentID.should.be.a( 'string' )
+      done()
+    })
+  })
+
+  it( 'should get the object status', (done) => {
+    deepgram.getObjectStatus( testContentId ).then( status => {
+      status.should.be.a( 'string' )
+      done()
+    })
+  })
+
+  it( 'should get the transcript', (done) => {
+    deepgram.getObjectTranscript( testContentId ).then( transcript => {
+      transcript.should.be.a( 'object' )
+      done()
+    })
+  }) 
+
+  it( 'should search objects', (done) => {
+    deepgram.objectSearch({
+      contentID: testContentId,
+      query: testContentQuery,
+      snippet: true
+    }).then( search => {
+      search.should.have.any.key( 'snippet' )
+      search.should.have.any.key( 'startTime' )
+      search.should.have.any.key( 'endTime' )
+      done()
+    })
+  })
+
 })
