8f1272ca50fee5f8650df65a4c16780d9419f4b6
Matias Insaurralde <matias@insaurral.de>
2016-03-16 02:27:04 -0300

    Babel adjustments & test environment

---
+++ test/unit/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+let app = express();
+let extended = { extended: false };
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded(extended));
+
+// parse application/json
+app.use(bodyParser.json(extended));
+
+app.all('/', (req, res, next) => {
+
+  /*
+  // HEAD request body must be 0 in length
+  if (req.method === 'HEAD' || req.method === 'OPTIONS') {
+    res.send(200, '');
+    return;
+  }
+  */
+
+  res.json({
+    message: 'OK'
+  });
+
+});
+
+app.get('/404-with-json-expected', (req, res, next) => {
+  res.sendStatus(404);
+});
+
+module.exports = app;
