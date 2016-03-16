b1bb9486f53e9f0f8575766880afbf285e312810
Matias Insaurralde <matias@insaurral.de>
2016-03-16 03:26:32 -0300

    Some features

--- src/deepgram.js
+++ src/deepgram.js
@@ -1,36 +1,50 @@
 import 'isomorphic-fetch'
 import Frisbee from 'frisbee'
 
-const methods = [
-  'get_balance',
-  'index_content',
-  'get_object_status',
-  'object_search',
-  'get_object_transcript'
-]
-
-
 export default class Deepgram {
   constructor( opts ) {
     this.opts = opts || {}
     this.api = new Frisbee({
-      baseURI: 'https://api.deepgram.com',
+      baseURI: 'http://api.deepgram.com',
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
     })
   }
 
-/*
-curl -H "Content-Type: application/json" -X POST -d '{ "action": "get_balance", "userID": "1458103900-1210f433-510f-4ea4-bbda-9b2e3fe28865-9829960392053284048220562727614" }' api.deepgram.com 
-*/
+  indexContent( dataUrl ) {
+    var payload = { action: 'index_content', data_url: dataUrl }
+    return this.api.post( '/', this._authenticatedPayload( payload ) )
+  }
 
   getBalance() {
-    console.log( 'get balance?', { action: 'get_balance', userID: this.opts.userId } )
-    this.api.post( '/', { action: 'get_balance', userID: this.opts.userId } ).then( data => {
-    }).catch( function(err) {
-console.log('err',err)
-    })
+    var payload = { action: 'get_balance' }
+    return this.api.post( '/', this._authenticatedPayload( payload ) )
+  }
+
+  getObjectStatus( contentID ) {
+    var payload = { action: 'get_object_status', contentID: contentID }
+    return this.api.post( '/', this._authenticatedPayload( payload ) )
+  }
+
+  getObjectTranscript( contentID ) {
+    var payload = { action: 'get_object_transcript', contentID: contentID }
+    return this.api.post( '/', this._authenticatedPayload( payload ) )
+  }
+
+  objectSearch( query ) {
+    console.log( 'objectsearch', query )
+
+    var payload = query || { }
+    payload.action = 'object_search'
+
+    return this.api.post( '/', this._authenticatedPayload( payload ) )
+  }
+
+  _authenticatedPayload( object ) {
+    var wrapper = {}
+    object.userID = this.opts.userId
+    wrapper.body = JSON.stringify( object )
+    return wrapper
   }
 }
